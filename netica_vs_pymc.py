b47ef317b86494c20525f75404c39b46756b2361
cedavidyang <ynyang1988@yahoo.com>
2016-08-10 09:04:03 +0800

    use rvs for postprocessing

--- netica_vs_pymc.py
+++ netica_vs_pymc.py
@@ -5,11 +5,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # load data
+# with np.load('bndiscrete20std0.npz') as data:
 with np.load('bndiscrete10.npz') as data:
     r4beliefs = data['r4beliefs']
     r5beliefs = data['r5beliefs']
     r4bins = data['r4bins']
     r5bins = data['r5bins']
+    # r4rep = data['r4means']
+    # r5rep = data['r5means']
     r4rvs = data['r4rvs']
     r5rvs = data['r5rvs']
 db=pymc.database.pickle.load('pymc2_straub2010ex1_m45_50_100.pickle')
@@ -20,6 +23,8 @@ db.close()
 # test
 r4rep = [rv.stats('m')[()] for rv in r4rvs]
 r5rep = [rv.stats('m')[()] for rv in r5rvs]
+r4mean = np.dot(r4rep, r4beliefs)
+r5mean = np.dot(r5rep, r5beliefs)
 
 # plot
 # posterior r4
